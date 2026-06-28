3a36cfd6b38fafe0bff8626181270af3f5b43b20
Ajith <Ajithesh.ai@gmail.com>
2026-06-28 12:07:53 +0530

    Added hello.py and deleted .gitkeep

---
+++ Python/Day_01/hello.py
@@ -0,0 +1,15 @@
+name = 'V Ajithesh Chowdary'
+company = 'Microsoft'
+goal = 'Become an AI Engineer'
+
+print("Name:", name, "Company:", company, "Goal:", goal)
+
+print()
+print("==================================")
+print("    AI ENGINEER PROFILE     ")
+print("==================================")
+print()
+print("Name      :", name)
+print("Company   :", company)
+print("Goal      :", goal)
+print("==================================")
